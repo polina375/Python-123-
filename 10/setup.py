b28b65fa2956f957fa8c4e496f9adf6bac628127
polina375 <apollinariya375@gmail.com>
2025-12-20 13:37:13 +0300

    Update 4.4

--- 10/setup.py
+++ 10/setup.py
@@ -6,7 +6,6 @@ setup(
     ext_modules=cythonize(
         [
             "integration_cython.pyx",
-            "integration_cython_opt.pyx"
         ],
         annotate=True,  # Генерируем аннотации для ВСЕХ файлов
         language_level="3"
